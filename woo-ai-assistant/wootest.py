4184dd62b9f25dcb7dc5f4bbe393eb17b29a32b6
Manuhd <41427184+Manuhd@users.noreply.github.com>
2025-10-18 01:01:42 +0530

    Obfuscate API credentials in wootest.py
    Updated consumer_key and consumer_secret to obfuscate sensitive information.

--- woo-ai-assistant/wootest.py
+++ woo-ai-assistant/wootest.py
@@ -3,8 +3,8 @@ from requests.auth import HTTPBasicAuth
 
 url = "http://ecomerce-agentic-ai.local/wp-json/wc/v3/products"
 
-consumer_key = "ck_8f6b99a774aed822c10549132fa9f05cf1dfb119"
-consumer_secret = "cs_d38a8071cdc97e77d3418ecacc61066d0ef5f010"
+consumer_key = "ck_xxxxxxxxxxxdfb119"
+consumer_secret = "cs_xxxxxxxxx5f010"
 
 response = requests.get(url, auth=HTTPBasicAuth(consumer_key, consumer_secret))
 
@@ -12,3 +12,4 @@ if response.status_code == 200:
     print(response.json())
 else:
     print("Error:", response.status_code, response.text)
+
